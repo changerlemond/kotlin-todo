e71248eb0af87cffae4df954fcc5d0c9239ff501
changerlemond <83348294+changerlemond@users.noreply.github.com>
2024-04-28 13:16:11 +0900

    chore: remove unused code

--- src/main/kotlin/server/project/auth/JwtAuthenticationFilter.kt
+++ src/main/kotlin/server/project/auth/JwtAuthenticationFilter.kt
@@ -3,7 +3,6 @@ package server.project.auth
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
@@ -23,8 +22,6 @@ class JwtAuthenticationFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        val log = LoggerFactory.getLogger(JwtAuthenticationFilter::class.java.name)
-
         val authHeader = request.getHeader(HttpHeaders.AUTHORIZATION)
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
